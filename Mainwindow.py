96102cabc836ea449bbb9ca1245b8e232c7ad998
scarletspeedster-2 <keplar62e@gmail.com>
2023-03-23 07:41:48 +0530

    files

---
+++ Mainwindow.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt6.QtWidgets import QApplication,QMainWindow,QPushButton
+import subprocess
+class Main_window(QMainWindow):
+
+    def __int__(self):
+        super.__init__()
+
